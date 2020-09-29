145ab8e474d8a7df319c74ab3f88f133559b0ef6
Eric Kuijpers <egm.kuijpers1@avans.nl>
2020-09-30 00:20:26 +0200

    Seed Data, opleuken met plaatjes enzo

--- SuperSushi.Data/SuperSushiContext.cs
+++ SuperSushi.Data/SuperSushiContext.cs
@@ -34,6 +34,25 @@ namespace SuperSushi.Data
                 .HasOne(pt => pt.Gerecht)
                 .WithMany(t => t.Onderdeelvan)
                 .HasForeignKey(pt => pt.GerechtId);
+
+
+            modelBuilder.Entity<Menu>().HasData(new Menu { Id = 1, Naam = "Smells Fishy" });
+            modelBuilder.Entity<Menu>().HasData(new Menu { Id = 2, Naam = "Bunny Bugs" });
+            modelBuilder.Entity<Menu>().HasData(new Menu { Id = 3, Naam = "Mighty Meaty" });
+
+            modelBuilder.Entity<Gerecht>().HasData(new Gerecht { Id = 1, Omschrijving = "Maki Tuna", Soort = Soort.Vis });
+            modelBuilder.Entity<Gerecht>().HasData(new Gerecht { Id = 2, Omschrijving = "Sashimi Salmon", Soort = Soort.Vis });
+
+            modelBuilder.Entity<Gerecht>().HasData(new Gerecht { Id = 3, Omschrijving = "Caramelised Crickets ", Soort = Soort.Insect });
+            modelBuilder.Entity<Gerecht>().HasData(new Gerecht { Id = 4, Omschrijving = "Cinnamon Worms", Soort = Soort.Insect });
+
+            modelBuilder.Entity<Gerecht>().HasData(new Gerecht { Id = 5, Omschrijving = "Nigiri Tofu", Soort = Soort.Vega });
+            modelBuilder.Entity<Gerecht>().HasData(new Gerecht { Id = 6, Omschrijving = "Gunkam Zeewier", Soort = Soort.Vega });
+
+            modelBuilder.Entity<Gerecht>().HasData(new Gerecht { Id = 7, Omschrijving = "Tebasaki Kip", Soort = Soort.Vlees });
+            modelBuilder.Entity<Gerecht>().HasData(new Gerecht { Id = 8, Omschrijving = "Teriyaki Biefstuk", Soort = Soort.Vlees });
+
         }
+
     }
 }
